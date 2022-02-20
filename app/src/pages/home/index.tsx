09364a06419bb88ee515bdc0d7a528c8d838056c
Igor Morais <igor.morais@hollowsoft.com>
2022-02-20 17:41:56 -0300

    implement home page stub

---
+++ app/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react'
+
+type HomeProps = {
+
+}
+
+const Home: FunctionComponent<HomeProps> = (props: HomeProps) => {
+  return (
+    <div />
+  )
+}
+
+export default Home
