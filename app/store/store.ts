aae260ef10cdfdac86a36cea89e1c087018f7127
Igor Morais <igor.morais@hollowsoft.com>
2022-11-22 14:24:56 -0300

    implement user store stub

--- app/store/store.ts
+++ app/store/store.ts
@@ -2,6 +2,7 @@ import { Instance } from 'mobx-state-tree'
 
 import { AuthStore } from './auth'
 import { FavStore } from './fav'
+import { UserStore } from './user'
 import { PostStore } from './post'
 import { BreedStore } from './breed'
 import { LocationStore } from './location'
@@ -9,6 +10,7 @@ import { LocationStore } from './location'
 export type Store = {
   auth: Instance<typeof AuthStore>
   fav: Instance<typeof FavStore>
+  user: Instance<typeof UserStore>
   post: Instance<typeof PostStore>
   breed: Instance<typeof BreedStore>
   location: Instance<typeof LocationStore>
@@ -17,6 +19,7 @@ export type Store = {
 export const store: Store = {
   auth: AuthStore.create(),
   fav: FavStore.create(),
+  user: UserStore.create(),
   post: PostStore.create(),
   breed: BreedStore.create(),
   location: LocationStore.create()
