af4bce073a680716e693b2292055464955425c1d
Igor Morais <igor.morais@hollowsoft.com>
2022-11-22 14:20:24 -0300

    implement breed store stub

---
+++ app/store/breed/index.ts
@@ -0,0 +1,9 @@
+import { types } from 'mobx-state-tree'
+
+export const BreedStore = types
+  .model({
+
+  })
+  .actions((self) => {
+
+  })
--- app/store/store.ts
+++ app/store/store.ts
@@ -1,14 +1,17 @@
 import { Instance } from 'mobx-state-tree'
 
 import { AuthStore } from './auth'
+import { BreedStore } from './breed'
 import { LocationStore } from './location'
 
 export type Store = {
   auth: Instance<typeof AuthStore>
+  breed: Instance<typeof BreedStore>
   location: Instance<typeof LocationStore>
 }
 
 export const store: Store = {
   auth: AuthStore.create(),
+  breed: BreedStore.create(),
   location: LocationStore.create()
 }
