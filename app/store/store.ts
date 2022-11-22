a7367d2d5ed71214151910e5bde9dba9fc2856b9
Igor Morais <igor.morais@hollowsoft.com>
2022-11-22 14:21:42 -0300

    implement fav store stub

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,17 +1,20 @@
 import { Instance } from 'mobx-state-tree'
 
 import { AuthStore } from './auth'
+import { FavStore } from './fav'
 import { BreedStore } from './breed'
 import { LocationStore } from './location'
 
 export type Store = {
   auth: Instance<typeof AuthStore>
+  fav: Instance<typeof FavStore>
   breed: Instance<typeof BreedStore>
   location: Instance<typeof LocationStore>
 }
 
 export const store: Store = {
   auth: AuthStore.create(),
+  fav: FavStore.create(),
   breed: BreedStore.create(),
   location: LocationStore.create()
 }
