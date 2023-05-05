b0db8634f4a9996b83418a2b6240a5239e6ae3f6
Igor Morais <igor.morais@hollowsoft.com>
2023-05-05 17:22:48 -0300

    add components to index

--- app/src/component/index.tsx
+++ app/src/component/index.tsx
@@ -1,7 +1,30 @@
+import { useToast } from './Toast'
+
+import { Avatar } from './Avatar'
 import { Button } from './Button'
+import { Footer } from './Footer'
+import { Header } from './Header'
+import { Image } from './Image'
 import { Input } from './Input'
+import { PostCard } from './PostCard'
+import { RangeSlider } from './RangeSlider'
+import { Search } from './Search'
+import { Spinner } from './Spinner'
+import { Swiper } from './Swiper'
+import { Switch } from './Switch'
 
 export {
+  Avatar,
   Button,
-  Input
+  Footer,
+  Header,
+  Image,
+  Input,
+  PostCard,
+  RangeSlider,
+  Search,
+  Spinner,
+  Swiper,
+  Switch,
+  useToast
 }
