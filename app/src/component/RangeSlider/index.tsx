c68059cbc7b7e105df404f169139d21af0abd987
Igor Morais <igor.morais@hollowsoft.com>
2023-05-05 17:22:05 -0300

    implement range slider component stub

---
+++ app/src/component/RangeSlider/index.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react'
+
+import { RangeSlider as Slider } from '@chakra-ui/react'
+
+type RangeSliderProps = {
+
+}
+
+export const RangeSlider: FunctionComponent<RangeSliderProps> = (props: RangeSliderProps) => {
+  return (
+    <Slider />
+  )
+}
