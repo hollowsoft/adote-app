6544eb0c0b4e3f5c4fbaeaf6884d43c876b68326
Igor Morais <igor.morais@hollowsoft.com>
2023-05-05 14:57:19 -0300

    implement button component stub

--- app/src/component/Button/index.tsx
+++ app/src/component/Button/index.tsx
@@ -1,11 +1,17 @@
 import { FunctionComponent } from 'react'
 
-type ButtonProps = {
+import { Button as Btn } from '@chakra-ui/react'
 
+type ButtonProps = {
+  children: string
 }
 
 export const Button: FunctionComponent<ButtonProps> = (props: ButtonProps) => {
+  const { children } = props
+
   return (
-    <button />
+    <Btn>
+      {children}
+    </Btn>
   )
 }
