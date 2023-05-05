7e31e3e9c22b4bb8a4194d6aedd900e2f5eed043
Igor Morais <igor.morais@hollowsoft.com>
2023-05-05 17:21:36 -0300

    implement search component stub

---
+++ app/src/component/Search/index.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from 'react'
+
+type SearchProps = {
+}
+
+export const Search: FunctionComponent<SearchProps> = (props: SearchProps) => {
+  return (
+    <div />
+  )
+}
