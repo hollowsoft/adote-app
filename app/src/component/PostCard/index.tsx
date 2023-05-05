a3c4c3ae7e6c49ae1cab6e00c083a02e52ce2f9d
Igor Morais <igor.morais@hollowsoft.com>
2023-05-05 17:22:13 -0300

    implement post card component stub

---
+++ app/src/component/PostCard/index.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from 'react'
+
+type PostCardProps = {
+}
+
+export const PostCard: FunctionComponent<PostCardProps> = (props: PostCardProps) => {
+  return (
+    <div />
+  )
+}
