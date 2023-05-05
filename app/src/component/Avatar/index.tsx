0b297925ff58470d57feb89f9fa67f60f0d1b02d
Igor Morais <igor.morais@hollowsoft.com>
2023-05-05 17:22:27 -0300

    implement avatar component stub

---
+++ app/src/component/Avatar/index.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react'
+
+import { Avatar as Av } from '@chakra-ui/react'
+
+type AvatarProps = {
+
+}
+
+export const Avatar: FunctionComponent<AvatarProps> = (props: AvatarProps) => {
+  return (
+    <Av />
+  )
+}
