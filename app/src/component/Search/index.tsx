7a4ecf9b1ac7b187b327d31757d5ab609779a973
Igor Morais <igor.morais@hollowsoft.com>
2023-05-05 18:10:09 -0300

    implement page structure

--- app/src/component/Search/index.tsx
+++ app/src/component/Search/index.tsx
@@ -1,10 +1,18 @@
 import { FunctionComponent } from 'react'
 
+import { Input } from '@chakra-ui/react'
+
 type SearchProps = {
+  search: string
+  onText?: (text: string) => void
 }
 
 export const Search: FunctionComponent<SearchProps> = (props: SearchProps) => {
+  const { search, onText } = props
+
   return (
-    <div />
+    <Input
+      value={search}
+      onChange={(e) => onText?.(e.target.value)} />
   )
 }
