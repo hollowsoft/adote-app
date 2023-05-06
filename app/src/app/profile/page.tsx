28a5342a19ddb08c36da149590353ef63483e354
Igor Morais <igor.morais@hollowsoft.com>
2023-05-06 20:34:08 -0300

    implement profile page structure

---
+++ app/src/app/profile/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { FunctionComponent } from 'react'
+
+type ProfilePageProps = {
+
+}
+
+const ProfilePage: FunctionComponent<ProfilePageProps> = (props: ProfilePageProps) => {
+  return (
+    <main>
+      Profile Page
+    </main>
+  )
+}
+
+export default ProfilePage
