08832b7e5d5ffba83b1981bb3ba1855323a7a4fd
Igor Morais <igor.morais@hollowsoft.com>
2023-05-06 20:25:53 -0300

    implement login pages structure

---
+++ app/src/app/login/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { FunctionComponent } from 'react'
+
+type LoginPageProps = {
+
+}
+
+const LoginPage: FunctionComponent<LoginPageProps> = (props: LoginPageProps) => {
+  return (
+    <main>
+      Login Page
+    </main>
+  )
+}
+
+export default LoginPage
