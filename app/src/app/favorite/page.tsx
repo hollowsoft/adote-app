d3caab8804ef452c7c887e56544f6a8c4497ecbe
Igor Morais <igor.morais@hollowsoft.com>
2023-05-06 20:34:01 -0300

    implement favorite page structure

---
+++ app/src/app/favorite/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { FunctionComponent } from 'react'
+
+type FavPageProps = {
+
+}
+
+const FavPage: FunctionComponent<FavPageProps> = (props: FavPageProps) => {
+  return (
+    <main>
+      Fav Page
+    </main>
+  )
+}
+
+export default FavPage
