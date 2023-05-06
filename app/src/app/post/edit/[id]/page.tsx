e6abce28ee91fee2801251b0e65d0dc2031ead63
Igor Morais <igor.morais@hollowsoft.com>
2023-05-06 19:58:25 -0300

    add post pages structure

---
+++ app/src/app/post/edit/[id]/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { FunctionComponent } from 'react'
+
+type EditPostPageProps = {
+
+}
+
+const EditPostPage: FunctionComponent<EditPostPageProps> = (props: EditPostPageProps) => {
+  return (
+    <main>
+      Edit Post Page
+    </main>
+  )
+}
+
+export default EditPostPage
